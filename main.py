d78fb29ed6db04d8bb83d44091bbbf9a18900d02
gfike19 <gfike19@gmail.com>
2016-09-15 15:28:19 -0500

    almost got build a blog working

--- main.py
+++ main.py
@@ -19,16 +19,17 @@ class Handler(webapp2.RequestHandler):
     def render (self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Home(Handler):
-    def get(self):
-        self.render("home.html")
-
-    def post(self):
-        action = self.request.get("action")
-        if action == "/newpost":
-            self.render("newpost.html")
-        if action == "/blog":
-            self.render("history.html")
+# class Home(Handler):
+#     def get(self):
+#         self.render("home.html")
+#
+#     def post(self):
+#         link = str(self.request.get("link"))
+#         if link == "Create a new blog":
+#             self.redirect("/newpost")
+#         if link == "View older blogs":
+#             self.redirect("/blog")
+#             self.e
 
 
 class Post(db.Model):
@@ -36,36 +37,44 @@ class Post(db.Model):
     blog_text = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
-class ViewPostHandler(Handler):
-    def get(self, id):
-        pass
+# class ViewPostHandler(Handler):
+#     def get(self, id):
+#         pass
 
 
 
 class NewPost(Handler):
+    def render_newblog(self, error = "", blog_title = "", blog_text = ""):
+        self.render("newpost.html", error = error, blog_title = blog_title, blog_text = blog_text)
+
     def get(self):
-        self.render("newpost.html")
+        self.render_newblog()
 
     def post(self):
         blog_title = self.request.get("blog_title")
         blog_text = self.request.get("blog_text")
 
         if blog_title and blog_text:
+            p = Post(blog_title = blog_title, blog_text = blog_text)
+            newpost = p.put()
+            # Post.put()
+            # p.put()
+            # Post.put(p)
+            # message = "Blog was successfully saved!"
+            # self.redirect('/blog')
+        else:
             error = "Fields cannot be empty"
-        p = Post(blog_title = blog_title, blog_text = blog_text)
-        p.put()
-        message = "Blog was successfully saved!"
-        self.redirect('/')
+            self.render_newblog(blog_title, blog_text, error)
 
 class History(Handler):
     def post(self):
-        posts = "SELECT * from Post" "ORDER BY created" "LIMIT 5"
+        posts = db.GqlQuery("SELECT post from Post ORDER BY created LIMIT 5")
         self.render("blog.html")
 
-webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+# webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 
 app = webapp2.WSGIApplication([
-    ('/', Home),
-    ('/blog', History),
-    ('/newpost', NewPost)
+    # ('/', Home),
+    ('/', NewPost),
+    ('/blog', History)
 ], debug = True)
